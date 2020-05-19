a55d4b7ae1a94d3a0826b25316300c44d17dc01e
d-d-wolfe <dave.wolfe623@gmail.com>
2020-05-18 21:11:56 -0700

    completed rendering books on show.ejs

--- server.js
+++ server.js
@@ -12,22 +12,52 @@ const app = express();
 
 // configs/middleware
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 app.get('/hello', (req, res) => {
   res.render('pages/index.ejs');
 });
 
-app.get('/new', (req, res) => {
+app.get('/searches/new', (req, res) => {
   res.render('pages/searches/new.ejs');
 });
 
+app.get('/searches/show', (req, res) =>{
+  res.render('pages/searches/show.ejs');
+});
+
 app.post('/searches/new', searchNewBook);
+//app.post('/searches/show', newBook);
 
 function searchNewBook(req, res) {
-  const url = 'https://www.googleapis.com/books/v1/volumes?q=';
-  console.log(req.body);
-  res.render('pages/searches/new');
+  const url = 'https://www.googleapis.com/books/v1/volumes';
+  //console.log(req.body.search);
+  const { keyword, type } = req.body.search;
+  let q = '';
+
+  if (type === 'title') {
+    q += `intitle:${keyword}`;
+  } else if (type === 'author') {
+    q += `inauthor:${keyword}`;
+  } else {
+    res.send({ message: 'must be title or author' });
+  }
+
+  superagent.get(url)
+    .query({ q })
+    .then((resultFromSuper) => {
+      const resultFromApi = resultFromSuper.body.items;
+      const newBook = resultFromApi.map(value => {
+        return new Book(value.volumeInfo);
+      });
+      //res.send(newBook);
+      res.render('pages/searches/show', {'newBook': newBook});
+    })
+    .catch(error => {
+      res.send(error);
+    });
+
 }
 
 // when we select title or author and we combine it with whatever the search string is in the text input, our URL that we get back is going to = request.body.search[0].
